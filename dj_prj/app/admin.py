13d90e72e633aa5cd8881255ddbb4042099df4a4
zdimon <zdimon@example.com>
2019-04-20 12:05:41 +0300

    first model

--- dj_prj/app/admin.py
+++ dj_prj/app/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import MainDocuments
 # Register your models here.
+
+class MainDocumentsAdmin(admin.ModelAdmin):
+    list_display = ['title', 'date']
+    search_fields = ['title']
+
+admin.site.register(MainDocuments, MainDocumentsAdmin)
--- dj_prj/app/models.py
+++ dj_prj/app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+
+class MainDocuments(models.Model):
+    uid = models.CharField(max_length=250)
+    title = models.TextField(blank=True, null=True)
+    href = models.CharField(max_length=250, blank=True, null=True)
+    date = models.DateField(blank=True, null=True)
+    is_error = models.BooleanField()
+    is_file_downloaded = models.BooleanField()
+    doc_html = models.TextField(blank=True, null=True)
+    doc_txt = models.TextField(blank=True, null=True)
+    is_document_downloaded = models.BooleanField()
+    is_files_downloaded = models.BooleanField()
+
+    class Meta:
+        managed = False
+        db_table = 'main_documents'
